adc0a7162168825434e523eb85aa39d56282caa4
71007792l <carloslue277@gmail.com>
2021-06-24 12:25:50 -0600

    cambiando tiitulo

--- resources/views/Administrador/reserva/create.blade.php
+++ resources/views/Administrador/reserva/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">Create Reserva</span>
+                        <span class="card-title">Crear reserva nueva</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('reservas.store') }}"  role="form" enctype="multipart/form-data">
